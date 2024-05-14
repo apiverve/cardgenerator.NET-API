c9b78c278c59e3ac2573f4163c3cd690e20fe921
APIVerve Team <hello@apiverve.com>
2024-05-14 15:21:19 -0500

    Bug fixes and Improvements

---
+++ cardgeneratorQueryOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class cardgeneratorQueryOptions {
+public string brand { get; set; }
+public int count { get; set; }
+}
+
+}
